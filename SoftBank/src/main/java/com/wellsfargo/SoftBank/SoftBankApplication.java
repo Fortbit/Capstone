3a6b3336884e78503d8f867f17bb61616d2fadc2
gsria <gsria@host.docker.internal>
2022-11-03 21:57:53 +0530

    Project Structure

---
+++ SoftBank/src/main/java/com/wellsfargo/SoftBank/SoftBankApplication.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.SoftBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftBankApplication.class, args);
+	}
+
+}
